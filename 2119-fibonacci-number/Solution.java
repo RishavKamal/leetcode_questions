0bfab519623310bf645dfd6c56edc1c53ac91a69
RISHAV KAMAL <56732972+RishavKamal@users.noreply.github.com>
2026-07-20 18:21:27 +0530

    LeetSync: add solution for fibonacci-number (v1)

---
+++ 2119-fibonacci-number/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        int first=0;
+        int second=1;
+        int next= 0;
+        for(int i=1; i<=n-1; i++){
+            next= first+second;
+            first=second;
+            second=next;
+        }return next;
+    }
+    
+}
