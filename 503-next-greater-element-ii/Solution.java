3ddf3413f89c9b1ce097f9a6b29f028637ea2693
RISHAV KAMAL <56732972+RishavKamal@users.noreply.github.com>
2026-09-17 23:50:02 +0530

    LeetSync: add solution for next-greater-element-ii (v1)

---
+++ 503-next-greater-element-ii/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int current = nums[i % n];
+            while (!stack.isEmpty() && stack.peek() <= current) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                result[i % n] = stack.peek();
+            }
+            stack.push(current);
+        }
+        return result;
+    }
+}
